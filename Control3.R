bb0b392ff69c02a6306fcade99c2a46e6ae5d94c
hugdelcur96 <47199487+hugdelcur96@users.noreply.github.com>
2019-10-30 13:48:27 -0600

    Add files via upload

--- Control3.R
+++ Control3.R
@@ -3,3 +3,4 @@ lab <- read_table2("http://allman.rhon.itam.mx/~ebarrios/EstApl2-2019/controles/
                    col_types = cols(Acid = col_factor(levels = c("B", "A")),
                                     Conc = col_factor(levels = c("L", "H")), 
                                     Temp = col_factor(levels = c("30", "60", "90"))))
+lab
