dbc573b3e786032f2364125305476037c599ea7d
hugdelcur96 <47199487+hugdelcur96@users.noreply.github.com>
2019-10-30 13:47:19 -0600

    Create Control3.R

---
+++ Control3.R
@@ -0,0 +1,5 @@
+library(tidyverse)
+lab <- read_table2("http://allman.rhon.itam.mx/~ebarrios/EstApl2-2019/controles/control3/bacterias.dat", 
+                   col_types = cols(Acid = col_factor(levels = c("B", "A")),
+                                    Conc = col_factor(levels = c("L", "H")), 
+                                    Temp = col_factor(levels = c("30", "60", "90"))))
