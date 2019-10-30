c7d5a48290dc2ea45b0a887ebbfb5ad352269a56
hugdelcur96 <47199487+hugdelcur96@users.noreply.github.com>
2019-10-30 14:33:35 -0600

    Esto solo es puro código

---
+++ Control 3/Control3.R
@@ -0,0 +1,18 @@
+library(tidyverse)
+bacterias <- read_table2(
+  "http://allman.rhon.itam.mx/~ebarrios/EstApl2-2019/controles/control3/bacterias.dat"
+  )
+bacterias$Temp <- as_factor(bacterias$Temp)
+bacterias$Acid <- as_factor(bacterias$Acid)
+bacterias$Conc <- as_factor(bacterias$Conc)
+
+asac
+
+avaw
+
+ergioergeijrgije
+
+fwiewoifjwiofjewoei
+
+
+wfiuwfiuwfiu
